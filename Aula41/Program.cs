074f7474fa3bbb4bb3c97f7c30c711ac197e8fb7
Rafael Viana <rafael.viana@stc.ma.gov.br>
2021-03-21 21:40:10 -0300

    projeto-teste-dell

---
+++ Aula41/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Aula41 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            //teste de novo arquivo
+        }
+    }
+}
